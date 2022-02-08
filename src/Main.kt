3e99147f08e1244b7005e9d30abbca6e732d9d6c
Al Jette <ajette@students.mchenry.edu>
2022-02-08 08:35:31 -0600

    Updated commit of MAD-105 Classes Assignment

--- src/Main.kt
+++ src/Main.kt
@@ -6,21 +6,18 @@ fun main(args: Array<String>) {
     var emp2 = employee(2,"Peg", "Bundy", 2)
     var emp3 = employee(3, "Steve", "Rhodes", 3)
 
-    // Print info about all 3 employees in different ways.  emp1 via Class/printAll,
-    // Print emp2 Full Name via Full_Name function call within Class
+    // Print info about all 3 employees in different ways.
+    // Print emp1 via Class/printAll,
+    // Print emp2 Full Name via Full_Name call within employee class
     // Print emp3 Full Name and directly print emp3.shift.
     emp1.printAll()
     println(emp2.Full_Name())
     println(emp3.Full_Name() + "  shift: " + emp3.shift)
 
     // change_shift for emp1 and re-print all info for emp1
- //   emp1.shift = 8
+    // emp1.shift = 8 => commented out, but this does the exact same as the call below
     emp1.change_shift(8)
     emp1.printAll()
-
-    //how to loop through emp[$count] to print info?
-    var count: Int = 1
-
 
     println("Program arguments: ${args.joinToString()}")
 }
